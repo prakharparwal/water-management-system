c90ec5ef8d4d3e2b87a389d6d63eb6ff827369da
Prakhar Parwal <prakhar.parwal@gmail.com>
2023-03-02 19:30:25 +0530

    WMS system

---
+++ src/test/java/com/fabric/water/management/system/WaterManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabric.water.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
